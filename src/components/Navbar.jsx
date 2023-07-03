7eab33dbe43b34797d475699adadd8306c3231bf
Abu Abdullah <abdullah72308@gmail.com>
2023-07-03 09:37:09 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+const Navbar = () => {
+    return (
+        <Header
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+            }}
+        >
+            <div className="demo-logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                items={new Array(15).fill(null).map((_, index) => {
+                    const key = index + 1;
+                    return {
+                        key,
+                        label: `nav ${key}`,
+                    };
+                })}
+            />
+        </Header>
+    );
+};
+
+export default Navbar;
